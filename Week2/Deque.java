b37a5da6fd72b579deb3d5f03354c7f11159ed07
Christina Owens <owens.christina@gmail.com>
2018-12-13 13:57:48 -0800

    Create Deque program without constructor and 'Iterable' implementation

--- Week2/Deque.java
+++ Week2/Deque.java
@@ -1,49 +1,78 @@
-public class Deque<Item> implements Iterable<Item> {
-    // construct an empty deque
-    public Deque() {
-        class Node() {
-            String item;
-            Node next;
-        }
-    }
+import edu.princeton.cs.algs4.StdOut;
+
+public class Deque<Item> { //implements Iterable<Item> {
     
-    // is the deque empty?
-    public boolean isEmpty() {
-        
-    }
+   private int size = 0;
+   private Node first, penult, last;
+   
+   // inner class
+   private class Node {
+      Item item;
+      Node next;
+   }
+   
+   // construct an empty deque
+//   public Deque() {
+//      Deque test = new Deque();
+//   }
     
-    // return the number of items on the deque
-    public int size() {
-        
-    }
+   // is the deque empty?
+   public boolean isEmpty() {
+      return first == null;
+   }
     
-    // add the item to the front
-    public void addFirst(Item item) {
-        
-    }
+   // return the number of items on the deque
+   public int size() {
+      return size;
+   }
     
-    // add the item to the end
-    public void addLast(Item item) {
-        
-    }
+   // add the item to the front (push)
+   public void addFirst(Item item) {
+      Node oldfirst = first;
+      first = new Node();
+      first.item = item;
+      first.next = oldfirst;
+      size++;
+   }
     
-    // remove and return the item from the front
-    public Item removeFirst() {
-        
-    }
+   // add the item to the end (enqueue)
+   public void addLast(Item item) {
+      Node oldlast = last;
+      last = new Node();
+      last.item = item;
+      last.next = null;
+      if (isEmpty()) first = last;
+      else oldlast.next = last;
+      size++;
+   }
     
-    // remove and return the item from the end
-    public Item removeLast() {
-        
-    }
+   // remove and return the item from the front (dequeue)
+   public Item removeFirst() {
+      Item item = first.item;
+      first = first.next;       
+      if (isEmpty()) last = null;
+      size--;
+      return item;
+   }
     
-    // return an iterator over items in order from front to end
-    public Iterator<Item> iterator() {
-        
-    }
+   // remove and return the item from the end
+   public Item removeLast() {
+      Item item = last.item;
+      first = first.next;
+      if (isEmpty()) last = null;
+      size--;
+      return item;
+   }
     
-    // unit testing (optional)
-    public static void main(String[] args) {
-        
-    }
+   // return an iterator over items in order from front to end
+//   public Iterator<Item> iterator() {
+//        
+//   }
+    
+   // unit testing (optional)
+   public static void main(String[] args) {
+       Deque test = new Deque();
+       StdOut.println("Deque empty? " + test.isEmpty());
+       StdOut.println("Deque size:  " + test.size());
+   }
 }
