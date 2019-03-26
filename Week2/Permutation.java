a99c6c74bd284c039aa77441382ac85dbbfd2d26
Christina Owens <owens.christina@gmail.com>
2019-03-25 22:58:05 -0700

    Update local repo with current state of all files in preparation for push to newly created remote repo

--- Week2/Permutation.java
+++ Week2/Permutation.java
@@ -1,15 +1,23 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
    public static void main(String[] args) {
       int k = Integer.parseInt(args[0].substring(0, 1));
       int n = 0;
       RandomizedQueue<String> queue = new RandomizedQueue<String>();
-      while (!StdIn.isEmpty()) {
-         String item = StdIn.readString();
-         queue.enqueue(item);
+//      while (!StdIn.isEmpty()) {
+//         String item = StdIn.readString();
+//         queue.enqueue(item);
+//      }
+      
+      while (queue.size() < k) {
+         String[] words = StdIn.readAllStrings();
+         queue.enqueue(words[StdRandom.uniform(words.length)]);
       }
+         
+      
       while (n < k) {
          // This doesn't help the RandomizedQueue 'null' problem
          // when RQ gets tested alone.
