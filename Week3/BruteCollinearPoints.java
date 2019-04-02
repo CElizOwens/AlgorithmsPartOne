33134b8ffa54909f4c095e67f2d29dc2563a9175
Christina Owens <owens.christina@gmail.com>
2019-04-02 16:10:22 -0700

    Complete BruteCollinearPoints class successful unittests

---
+++ Week3/BruteCollinearPoints.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+	private int counter = 0;
+	private LineSegment[] holder;
+	private LineSegment[] results;
+	private Point[] sorted;
+
+	public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
+		holder = new LineSegment[points.length];
+
+		if (points == null) throw new IllegalArgumentException("Cannot be null");
+		// if a point in points is null -- done
+		// or a point is repeat twice
+		// return IllegalArgumentException
+
+	    for (int i = 0; i < points.length - 3; i++) {
+	    	for (int j = i + 1; j < points.length - 2; j++) {
+	    		for (int k = j + 1; k < points.length - 1; k++) {
+	    			for (int l = k + 1; l < points.length; l++) {
+				    	if (points[l] == null) throw new IllegalArgumentException("Cannot be null");
+				    	if (points[l] == points[i] || points[l] == points[j] || points[l] == points[k]) {
+				    		throw new IllegalArgumentException("Repeated point");
+				    	}
+				    	if (points[i].slopeTo(points[j]) == (points[i].slopeTo(points[k]))) {
+				    		if (points[i].slopeTo(points[l]) == (points[i].slopeTo(points[k]))) {
+				    			counter++;
+				    			// sort these points from min to max
+				    			sorted = new Point[] { points[i], points[j], points[k], points[l] };
+				    			Arrays.sort(sorted);
+				    			// put this line segment in a LineSegment array
+				    			holder[counter - 1] = new LineSegment(sorted[0], sorted[3]);
+				    		}
+				    	}
+	    			}
+	    		}
+	    	}
+    	}
+
+	}
+
+	public int numberOfSegments() { // the number of line segments
+		return counter;
+	}
+
+	public LineSegment[] segments() { // the line segments
+		results = new LineSegment[counter];
+		int x = 0;
+		while (holder[x] != null) {
+			results[x] = holder[x];
+			x++;
+		}
+		return results;
+	}
+}
--- Week3/Pointtests.java
+++ Week3/Pointtests.java
@@ -1,7 +1,10 @@
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
 
 public class Pointtests {
+	private static Point[] test;
 
 	public static void main(String[] args) {
 
@@ -15,7 +18,15 @@ public class Pointtests {
 	    Point eighth = new Point(4, 2);
 	    Point ninth = new Point(6, 3);
 
-	    Object[] night = new Object[]{second, third};
+
+	    Point tenth = new Point(6, 4);
+	    Point eleventh = new Point(7, 5);
+
+
+	    test = new Point[] { eighth, first, tenth, eleventh };
+
+	    // ++ Not sure about this next line: ++
+	    // Object[] night = new Object[]{second, third};
 
 		// Test 'compareTo()'
 	 //    StdOut.println(first.toString() + second.toString());
@@ -30,16 +41,53 @@ public class Pointtests {
 	 //    StdOut.println(first.compareTo(sixth) + ": answer is -1");
 
 		// Test 'slopeTo()'
-	    StdOut.println(first.slopeTo(second) + ": answer is negative infinity");
-	    StdOut.println(first.slopeTo(third) + ": answer is positive infinity");
-	    StdOut.println(first.slopeTo(fifth) + ": answer is +0.0");
-	    StdOut.println(first.slopeTo(seventh) + ": answer is a valid slope value");
-	    StdOut.println(first.slopeTo(eighth) + ": answer is a valid slope value");
+	    // StdOut.println(first.slopeTo(second) + ": answer is negative infinity");
+	    // StdOut.println(first.slopeTo(third) + ": answer is positive infinity");
+	    // StdOut.println(first.slopeTo(fifth) + ": answer is +0.0");
+	    // StdOut.println(first.slopeTo(seventh) + ": answer is a valid slope value");
+	    // StdOut.println(first.slopeTo(eighth) + ": answer is a valid slope value");
 
 	    // Test 'slopeOrder()'
-		StdOut.println(first.slopeOrder().compare(second, fifth) + ": answer is -1");
-		StdOut.println(first.slopeOrder().compare(third, fifth) + ": answer is 1");
-		StdOut.println(first.slopeOrder().compare(sixth, ninth) + ": answer is 0");
+		// StdOut.println(first.slopeOrder().compare(second, fifth) + ": answer is -1");
+		// StdOut.println(first.slopeOrder().compare(third, fifth) + ": answer is 1");
+		// StdOut.println(first.slopeOrder().compare(sixth, ninth) + ": answer is 0");
+
+		// Test 'LineSegment.java'
+		// Point high = new Point(4096, 20992);
+		// Point low = new Point(0, 0);
+		// LineSegment seg = new LineSegment(high, low);
+		// seg.draw();
+
+	    // Test 'BruteCollinearPoints.java'
+	    // BruteCollinearPoints trial = new BruteCollinearPoints(test);
+
+	    // read the n points from a file
+	    In in = new In(args[0]);
+	    int n = in.readInt();
+	    Point[] points = new Point[n];
+	    for (int i = 0; i < n; i++) {
+	        int x = in.readInt();
+	        int y = in.readInt();
+	        points[i] = new Point(x, y);
+	    }
+
+	    // draw the points
+	    StdDraw.enableDoubleBuffering();
+	    StdDraw.setXscale(0, 32768);
+	    StdDraw.setYscale(0, 32768);
+	    for (Point p : points) {
+	        p.draw();
+	    }
+	    StdDraw.show();
+
+	    // print and draw the line segments
+		BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+
+	    for (LineSegment segment : collinear.segments()) {
+        	StdOut.println(segment);
+	        segment.draw();
+		}
+		StdDraw.show();
 
 	}
 }
